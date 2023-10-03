f6bdd3045a978789926762b0481f767471a960ee
Hữu Phúc <20521770@gm.uit.edu.vn>
2023-10-03 15:40:27 +0700

    a little row__posters

---
+++ src/Components/Row.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import axios from "../axios";
+
+function Row({ title, fetchUrl }) {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        // if [], run once when the row loads, and don't run again.
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+    }, [fetchUrl]);
+
+    console.table(movies);
+
+    return (
+        <div className="row">
+            <h2>{title}</h2>
+
+            <div className="row__posters">
+                {movies.map((movie) => (
+                    <img src={movie.backdrop_path} alt={movie.name} />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Row;
